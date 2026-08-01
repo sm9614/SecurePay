c84e48db2f9655ae2941debffa122740f5baaa4a
sm9614 <sm9614@rit.edu>
2026-07-31 22:18:02 -0400

    Add JWT support and JwtService
    - Updated pom.xml with jwt
    
    - Created JwtService

---
+++ payment-platform/src/main/java/com/pm/paymentplatform/security/JwtService.java
@@ -0,0 +1,61 @@
+package com.pm.paymentplatform.security;
+
+import com.pm.paymentplatform.merchant.Merchant;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.UUID;
+
+@Service
+public class JwtService {
+
+    private final SecretKey secret;
+    private final long expiration;
+
+    public JwtService(@Value("${jwt.secret}") String secret,
+                      @Value("${jwt.expiration-ms}") long expiration ) {
+        this.secret = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+        this.expiration = expiration;
+    }
+
+    public String generateToken(Merchant merchant) {
+        return Jwts.builder()
+                .subject(String.valueOf(merchant.getId()))
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(secret)
+                .compact();
+    }
+
+    public UUID extractMerchantId(String token) {
+        return UUID.fromString(
+                Jwts.parser()
+                .verifyWith(secret)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject()
+        );
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+             Jwts.parser()
+            .verifyWith(secret)
+            .build()
+            .parseSignedClaims(token);
+
+            return true;
+
+        }catch (JwtException e) {
+            return false;
+        }
+    }
+
+}
